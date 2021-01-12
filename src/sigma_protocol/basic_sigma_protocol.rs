a770230bd99757aeaa68f2fc472ad01e4d1f0691
root <287494524@qq.com>
2021-01-12 14:34:56 +0800

    basic Pi_0 protocol

--- src/sigma_protocol/basic_sigma_protocol.rs
+++ src/sigma_protocol/basic_sigma_protocol.rs
@@ -7,61 +7,106 @@ use super::super::commitments::{Commitments, MultiCommitGens};
 use super::super::errors::ProofVerifyError;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct KnowledgeProof {
-  alpha: CompressedGroup,
-  z1: Scalar,
-  z2: Scalar,
+//basic sigma protocol: $\Pi_0$-protocol
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Basic_Pi_0_Proof {
+  A: CompressedGroup,
+  z: Vec<Scalar>,
+  y_p: Scalar, // y'
+  blind_z: Scalar,
 }
 
-impl KnowledgeProof {
+impl Basic_Pi_0_Proof {
   fn protocol_name() -> &'static [u8] {
-    b"knowledge proof"
+    b"basic pi_0 proof"
+  }
+
+  // compute linear form $y=L(\vec{x})$
+  pub fn compute_linearform(a: &[Scalar], b: &[Scalar]) -> Scalar {
+    assert_eq!(a.len(), b.len());
+    (0..a.len()).map(|i| a[i] * b[i]).sum()
   }
 
   pub fn prove(
+    gens_1: &MultiCommitGens,
     gens_n: &MultiCommitGens,
     transcript: &mut Transcript,
     random_tape: &mut RandomTape,
-    x: &Scalar,
-    r: &Scalar,
-  ) -> (KnowledgeProof, CompressedGroup) {
-    transcript.append_protocol_name(KnowledgeProof::protocol_name());
+    x_vec: &[Scalar],
+    blind_x: &Scalar,
+    a_vec: &[Scalar],
+  ) -> (Basic_Pi_0_Proof, CompressedGroup, Scalar) {
+    transcript.append_protocol_name(Basic_Pi_0_Proof::protocol_name());
 
-    // produce two random Scalars
-    let t1 = random_tape.random_scalar(b"t1");
-    let t2 = random_tape.random_scalar(b"t2");
+    let n = x_vec.len();
+    assert_eq!(x_vec.len(), a_vec.len());
+    assert_eq!(gens_n.n, a_vec.len());
+    assert_eq!(gens_1.n, 1);
 
-    let C = x.commit(&r, gens_n).compress();
-    C.append_to_transcript(b"C", transcript);
+    // produce randomness for the proofs
+    let r_vec = random_tape.random_vector(b"r_vec", n);
+    let blind_r = random_tape.random_scalar(b"blind_r");
+    //let r_beta = random_tape.random_scalar(b"r_beta");
 
-    let alpha = t1.commit(&t2, gens_n).compress();
-    alpha.append_to_transcript(b"alpha", transcript);
+    let Cx = x_vec.commit(&blind_x, gens_n).compress();
+    Cx.append_to_transcript(b"Cx", transcript);
+
+    let y = Basic_Pi_0_Proof::compute_linearform(&a_vec, &x_vec);
+    y.append_to_transcript(b"y", transcript);
+
+    let A = r_vec.commit(&blind_r, gens_n).compress();
+    A.append_to_transcript(b"A", transcript);
+
+    let y_p = Basic_Pi_0_Proof::compute_linearform(&a_vec, &r_vec);
+    y_p.append_to_transcript(b"y_p", transcript);
 
     let c = transcript.challenge_scalar(b"c");
 
-    let z1 = x * c + t1;
-    let z2 = r * c + t2;
+    let z = (0..r_vec.len())
+      .map(|i| c * x_vec[i] + r_vec[i])
+      .collect::<Vec<Scalar>>();
 
-    (KnowledgeProof { alpha, z1, z2 }, C)
+    let blind_z = c * blind_x + blind_r;
+
+    (
+      Basic_Pi_0_Proof {
+        A,
+        z,
+        y_p,
+        blind_z,
+      },
+      Cx,
+      y,
+    )
   }
 
   pub fn verify(
     &self,
+    gens_1: &MultiCommitGens,
     gens_n: &MultiCommitGens,
     transcript: &mut Transcript,
-    C: &CompressedGroup,
+    a: &[Scalar],
+    Cx: &CompressedGroup,
+    y: &Scalar,
   ) -> Result<(), ProofVerifyError> {
-    transcript.append_protocol_name(KnowledgeProof::protocol_name());
-    C.append_to_transcript(b"C", transcript);
-    self.alpha.append_to_transcript(b"alpha", transcript);
+    assert_eq!(gens_n.n, a.len());
+    assert_eq!(gens_1.n, 1);
+
+    transcript.append_protocol_name(Basic_Pi_0_Proof::protocol_name());
+    Cx.append_to_transcript(b"Cx", transcript);
+    y.append_to_transcript(b"y", transcript);
+    self.A.append_to_transcript(b"A", transcript);
+    self.y_p.append_to_transcript(b"y_p", transcript);
 
     let c = transcript.challenge_scalar(b"c");
 
-    let lhs = self.z1.commit(&self.z2, gens_n).compress();
-    let rhs = (c * C.unpack()? + self.alpha.unpack()?).compress();
+    let mut result =
+      c * Cx.unpack()? + self.A.unpack()? == self.z.commit(&self.blind_z, gens_n);
 
-    if lhs == rhs {
+    let l_z = Basic_Pi_0_Proof::compute_linearform(&self.z, &a);
+    result &= c * y + self.y_p == l_z;
+
+    if result {
       Ok(())
     } else {
       Err(ProofVerifyError::InternalError)
@@ -75,22 +120,39 @@ mod tests {
   use super::*;
   use rand::rngs::OsRng;
   #[test]
-  fn check_knowledgeproof() {
+  fn check_basic_pi_0_proof() {
     let mut csprng: OsRng = OsRng;
 
-    let gens_1 = MultiCommitGens::new(1, b"test-knowledgeproof");
+    let n = 1024;
 
-    let x = Scalar::random(&mut csprng);
-    let r = Scalar::random(&mut csprng);
+    let gens_1 = MultiCommitGens::new(1, b"test-two");
+    let gens_1024 = MultiCommitGens::new(n, b"test-1024");
 
+    let mut x: Vec<Scalar> = Vec::new();
+    let mut a: Vec<Scalar> = Vec::new();
+    for _ in 0..n {
+      x.push(Scalar::random(&mut csprng));
+      a.push(Scalar::random(&mut csprng));
+    }
+
+    let r_x = Scalar::random(&mut csprng);
+    
     let mut random_tape = RandomTape::new(b"proof");
     let mut prover_transcript = Transcript::new(b"example");
-    let (proof, committed_value) =
-      KnowledgeProof::prove(&gens_1, &mut prover_transcript, &mut random_tape, &x, &r);
-
+    let (proof, Cx, y) = Basic_Pi_0_Proof::prove(
+      &gens_1,
+      &gens_1024,
+      &mut prover_transcript,
+      &mut random_tape,
+      &x,
+      &r_x,
+      &a,
+    );
+    
     let mut verifier_transcript = Transcript::new(b"example");
     assert!(proof
-      .verify(&gens_1, &mut verifier_transcript, &committed_value)
+      .verify(&gens_1, &gens_1024, &mut verifier_transcript, &a, &Cx, &y)
       .is_ok());
   }
+
 }
