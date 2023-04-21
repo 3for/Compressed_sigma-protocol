71ad485e6c1ab9e89d65e07f48c7a95bf83211cf
蔡廷漳 <1019386658@qq.com>
2023-04-21 20:01:09 +0800

    fix protocol 7 weak fiat-shamir transformation
    Signed-off-by: 蔡廷漳 <1019386658@qq.com>

--- src/sigma_protocol/zk_protocol_7.rs
+++ src/sigma_protocol/zk_protocol_7.rs
@@ -1,15 +1,16 @@
-use super::super::transcript::{ProofTranscript};
-use super::super::scalar::Scalar;
-use super::super::group::{CompressedGroup};
-use merlin::Transcript;
-use super::super::random::RandomTape;
 use super::super::errors::ProofVerifyError;
-use serde::{Deserialize, Serialize};
-use super::super::nizk::{DotProductProofGens};
-use crate::sigma_protocol::zk_protocol_5::Pi_c_Proof;
+use super::super::group::CompressedGroup;
+use super::super::nizk::DotProductProofGens;
+use super::super::random::RandomTape;
+use super::super::scalar::Scalar;
+use super::super::transcript::ProofTranscript;
 use super::scalar_math;
 
-// // Protocol 4 in the paper: Compressed Proof of Knowledge $\Pi_OPEN$ 
+use crate::sigma_protocol::zk_protocol_5::Pi_c_Proof;
+use merlin::Transcript;
+use serde::{Deserialize, Serialize};
+
+// // Protocol 4 in the paper: Compressed Proof of Knowledge $\Pi_OPEN$
 #[derive(Debug, Serialize, Deserialize)]
 #[allow(non_camel_case_types)]
 pub struct Pi_NULLITY_Proof {
@@ -39,27 +40,13 @@ impl Pi_NULLITY_Proof {
     let rho_vec = scalar_math::vandemonde_challenge_one(rho, s);
     let l_matrix_t = scalar_math::matrix_transpose(&l_matrix);
     let l_vec = scalar_math::matrix_vector_mul(&l_matrix_t, &rho_vec);
-    
+
     let y = scalar_math::compute_linearform(&l_vec, &x_vec);
 
-    let (proof, P, P_hat) = Pi_c_Proof::prove(
-      &gens,
-      transcript,
-      random_tape,
-      x_vec,
-      gamma,
-      &l_vec,
-      &y,
-    );
+    let (proof, P, P_hat) =
+      Pi_c_Proof::prove(&gens, transcript, random_tape, x_vec, gamma, &l_vec, &y);
 
-    (
-      Pi_NULLITY_Proof{
-        proof,
-      },
-      P,
-      P_hat, 
-      y
-    )
+    (Pi_NULLITY_Proof { proof }, P, P_hat, y)
   }
 
   pub fn verify(
@@ -75,30 +62,24 @@ impl Pi_NULLITY_Proof {
     assert!(gens.gens_n.n >= n);
 
     transcript.append_protocol_name(Pi_NULLITY_Proof::protocol_name());
-    
+
     let s = l_matrix.len();
     let rho = transcript.challenge_scalar(b"rho");
     let rho_vec = scalar_math::vandemonde_challenge_one(rho, s);
     let l_matrix_t = scalar_math::matrix_transpose(&l_matrix);
     let l_vec = scalar_math::matrix_vector_mul(&l_matrix_t, &rho_vec);
 
-    return self.proof.verify(
-      n,
-      &gens,
-      transcript,
-      &l_vec,
-      &P,
-      &y,
-      &P_hat,
-    );
+    return self
+      .proof
+      .verify(n, &gens, transcript, &l_vec, &P, &y, &P_hat);
   }
-
 }
-
 
 #[cfg(test)]
 mod tests {
   use super::*;
+  use crate::commitments::Commitments;
+  use crate::transcript::AppendToTranscript;
   use rand::rngs::OsRng;
   #[test]
   fn check_pi_nullity_proof() {
@@ -111,7 +92,7 @@ mod tests {
 
     let mut l_matrix: Vec<Vec<Scalar>> = Vec::new();
     let mut x_vec: Vec<Scalar> = Vec::new();
-    for _ in 0..n-1 {
+    for _ in 0..n - 1 {
       x_vec.push(Scalar::random(&mut csprng));
     }
 
@@ -119,18 +100,25 @@ mod tests {
     let tmp_x_vec = x_vec.clone();
     x_vec.push(-Scalar::one());
     for _ in 0..s {
-      let mut tmp: Vec<Scalar> = (0..n-1).map(|_| Scalar::random(&mut csprng)).collect();
+      let mut tmp: Vec<Scalar> = (0..n - 1).map(|_| Scalar::random(&mut csprng)).collect();
 
       //make $<L_i, \vec{x}>=0$
       let y = scalar_math::compute_linearform(&tmp, &tmp_x_vec);
-      tmp.push(y);      
+      tmp.push(y);
       l_matrix.push(tmp.clone());
 
-      assert_eq!(scalar_math::compute_linearform(&tmp, &x_vec), Scalar::zero());
+      assert_eq!(
+        scalar_math::compute_linearform(&tmp, &x_vec),
+        Scalar::zero()
+      );
     }
 
     let mut random_tape = RandomTape::new(b"proof");
     let mut prover_transcript = Transcript::new(b"example");
+
+    let P_secure = x_vec.commit(&gamma, &gens.gens_n).compress();
+    P_secure.append_to_transcript(b"P_secure", &mut prover_transcript);
+
     let (proof, P_vec, P_hat, y) = Pi_NULLITY_Proof::prove(
       &gens,
       &mut prover_transcript,
@@ -141,8 +129,19 @@ mod tests {
     );
 
     let mut verifier_transcript = Transcript::new(b"example");
+
+    P_vec.append_to_transcript(b"P_secure", &mut verifier_transcript);
+
     assert!(proof
-      .verify(n, &gens, &mut verifier_transcript, &l_matrix, &P_vec, &y, &P_hat)
+      .verify(
+        n,
+        &gens,
+        &mut verifier_transcript,
+        &l_matrix,
+        &P_vec,
+        &y,
+        &P_hat
+      )
       .is_ok());
   }
 }
